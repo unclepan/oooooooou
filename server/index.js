c007e360703e503ac895a5f80bcb7c9b267f618a
ooyang <ooyang@tencent.com>
2021-04-16 17:29:39 +0800

    fix(项目): 更改ip

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
   const {
-    host = process.env.HOST || '127.0.0.1',
+    host = process.env.HOST || '0.0.0.0',
     port = process.env.PORT || 3000
   } = nuxt.options.server
 
